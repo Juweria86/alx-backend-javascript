48ac024e4d37e39294f67b9cf2391b80f7db866a
Juweria86 <jujumelmi143@gmail.com>
2024-07-02 22:17:23 +0300

    Task4: Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+    return studentList
+    .filter(({ location }) => location === city)
+    .map((obj) => {
+      const xtargetNewGrade = newGrades.filter(({ studentId }) => studentId === obj.id);
+      const { grade = 'N/A' } = xtargetNewGrade.length > 0 ? xtargetNewGrade[0] : {};
+      return { ...obj, grade };
+    });
+}
