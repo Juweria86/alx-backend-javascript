34f4f7b0cfccb74528b222c6319fa5c28a2c88ca
Juweria86 <jujumelmi143@gmail.com>
2024-07-03 02:51:06 +0300

    Task8: solved some errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,10 @@
-export default function cleanSet(Set, startString) {
-  if (!Set || !startString || !(Set instanceof Set) || typeof startString !== 'string') {
+export default function cleanSet(setObj, startString) {
+  if (!setObj || !startString || !(setObj instanceof Set) || typeof startString !== 'string' || startString === '') {
     return '';
   }
-  return Array.from(Set)
+
+  return Array.from(setObj)
     .filter((ele) => ele && ele.startsWith(startString))
-    .map((ele) => ele.replace(startString, ''))
+    .map((ele) => ele.slice(startString.length))
     .join('-');
 }
